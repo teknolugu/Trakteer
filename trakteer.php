1b2c135cf1b5b56189f4f719c4c716d3c5fbecc3
Jovan Ferryal E. F <jovanzers@gmail.com>
2023-04-24 22:29:53 +0700

    Reduce error_log when no result from Trakteer

--- trakteer.php
+++ trakteer.php
@@ -29,12 +29,12 @@ $amount = $xpath->query('//*[@id="wrapper"]/div/div/div[3]/div[3]/div[2]');
 $adminFees = $xpath->query('//*[@id="wrapper"]/div/div/div[3]/div[3]/div[1]/div[4]/span[2]');
 
 $result = [
-    'OrderId' => $orderId[0]->nodeValue,
-    'OrderDate' => $orderDate[0]->nodeValue,
-    'PaymentMethod' => $paymentMethod[0]->nodeValue,
-    'CendolCount' => (int) preg_replace('/[^0-9]/', '', $cendol[0]->nodeValue),
-    'AdminFees' => (int) preg_replace('/[^0-9]/', '', $adminFees[0]->nodeValue),
-    'Total' => (int) preg_replace('/[^0-9]/', '', $amount[0]->nodeValue)
+    'OrderId' => @$orderId[0]->nodeValue,
+    'OrderDate' => @$orderDate[0]->nodeValue,
+    'PaymentMethod' => @$paymentMethod[0]->nodeValue,
+    'CendolCount' => (int) preg_replace('/[^0-9]/', '', @$cendol[0]->nodeValue),
+    'AdminFees' => (int) preg_replace('/[^0-9]/', '', @$adminFees[0]->nodeValue),
+    'Total' => (int) preg_replace('/[^0-9]/', '', @$amount[0]->nodeValue)
 ];
 
 header('Content-Type: application/json');
